2a0a615cd9cf39589f9699625c50a6a210d9361f
Jörg Oertel <joergoertel@proton.me>
2023-10-16 10:09:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Token Dependency For Security Project - 2023
---
+++ src/main/java/dk/lyngby/App.java
@@ -0,0 +1,14 @@
+package dk.lyngby;
+
+public class App {
+
+    static String name = "Hello world!";
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        getName();
+    }
+}
