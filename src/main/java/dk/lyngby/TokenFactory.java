c5ec2e38302b0377d621d3dc28efe41d4d509ad5
Jörg Oertel <joergoertel@proton.me>
2023-10-17 10:43:45 +0200

    refactored

--- src/main/java/dk/lyngby/TokenFactory.java
+++ src/main/java/dk/lyngby/TokenFactory.java
@@ -23,33 +23,4 @@ public class TokenFactory {
         return verifier.getUsernameFromToken(token);
     }
 
-//    public static void main(String[] args) throws TokenException {
-//        TokenCreator token = new TokenCreator();
-//        TokenVerifier verifier = new TokenVerifier();
-//        String secret = "secret841D8A6C80CBA4FCAD32D5367C18C53B";
-//
-//        ClaimBuilder claimBuilder = ClaimBuilder.builder()
-//                .issuer("lyngby")
-//                .audience("datamatiker")
-//                .claimSet(Map.of("username", "steve", "roles", "[ADMIN, USER]"))
-//                .expirationTime(3600000L)
-//                .issueTime(3600000L)
-//                .build();
-//
-//        ClaimBuilder claimBuilderVerify = ClaimBuilder.builder()
-//                .issuer("lyngby")
-//                .audience("datamatiker")
-//                .claimSet(Map.of("username", "steve", "roles", "[ADMIN, USER]"))
-//                .expirationTime(3600000L)
-//                .issueTime(3600000L)
-//                .build();
-//
-//        String token1 = token.createToken(claimBuilder, secret.getBytes());
-//
-//        String usernameFromToken = getUsernameFromToken(token1);
-//        System.out.println("Steve: " + usernameFromToken);
-//        System.out.println(token1);
-//        String verified = verifier.verifyTokenAndReturnClaim(token1, secret.getBytes(), claimBuilderVerify);
-//        System.out.println(verified);
-//    }
 }
